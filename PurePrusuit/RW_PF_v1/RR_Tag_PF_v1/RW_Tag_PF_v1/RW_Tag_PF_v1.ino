d401bf5328eaa8b3ade3c8bead3350a53af1f27b
HokieVPI <acheynet@vt.edu>
2026-02-10 19:01:25 -0500

    heading and path followinug

--- PurePrusuit/RW_PF_v1/RR_Tag_PF_v1/RW_Tag_PF_v1/RW_Tag_PF_v1.ino
+++ PurePrusuit/RW_PF_v1/RR_Tag_PF_v1/RW_Tag_PF_v1/RW_Tag_PF_v1.ino
@@ -1,53 +1,54 @@
 #include <PortentaUWBShield.h>
 #include <math.h>
+#include <PurePursuit.h>
 
 
 /**
- * This Script claculates the position of an Arduino Protenta Tag based 
- * on the distances to three anchors. Using multicast to range with 3+ anchors
- * UWB Ranging Controller (one-to-many)*
- * It expects multiple counterparts setup as Responders/Controlees*
+ * pure pursuit path following algorithm for a single tag
  */
 
 // Anchor Locations in Centimeters (x,y) z=0 
 const float Anchor1_x=0;// cm 
 const float Anchor1_y=0; // cm 
 const float Anchor2_x=0; // cm 
-const float Anchor2_y=213; // cm 
-const float Anchor3_x=182; // cm 
+const float Anchor2_y=467; // cm 
+const float Anchor3_x=506; // cm 
 const float Anchor3_y=0; // cm 
-
+// Initialize Distance Variables
+int dist_1 = 0;
+int dist_2 = 0;
+int dist_3 = 0; 
+// Anchor received state variables
 bool anchor1_received = false;
 bool anchor2_received = false;
 bool anchor3_received = false;
 //previous position state variable 
-float prevX = 0.0f;// cm 
-float prevY = 0.0f;// cm 
-float currentX = 0.0f;// cm 
-float currentY = 0.0f;// cm 
+double currentX_global = 0.0f;// cm 
+double currentY_global = 0.0f;// cm 
 bool prev_valid = false;
 
 // Heading State Variable 
-float Azimuth = 0.0f; // rad
-
+double Azimuth = 0.0f; // rad
+double global_azimuth = 0.0f; // rad
 
 // constants 
+static constexpr int HALF_CIRCULAR_BUFFER_SIZE = 5;
+static constexpr int CIRCULAR_BUFFER_SIZE = HALF_CIRCULAR_BUFFER_SIZE*2;
 const float MinMovement = 5.0f; // cm 
 const float minMovement_sq=MinMovement*MinMovement; // minimum movement squared
-float x_circular_buffer[5];
-float y_circular_buffer[5];
-static constexpr int CIRCULAR_BUFFER_SIZE = 5;
+float x_circular_buffer[CIRCULAR_BUFFER_SIZE];
+float y_circular_buffer[CIRCULAR_BUFFER_SIZE];
 int head_index = 0;
 int tail_index = CIRCULAR_BUFFER_SIZE-1;
 bool inRangingHandler = false;
 
 // Pure Prusuit var
-static constexpr int LOOK_AHEAD = 100; // cm
-static constexpr int INTERVAL = 100; // ms
-static constexpr int STOP_POINT = 10; // cm
+static constexpr int LOOK_AHEAD = 1000; // cm
+static constexpr int INTERVAL = 1000; // ms
+static constexpr int STOP_POINT = 50; // cm
 static constexpr int PATH_LEN = 4;
-static float pathX[PATH_LEN] = {0,100,200,300}; //waypoints in cm 
-static float pathY[PATH_LEN] = {0,100,200,300}; // waypoints in cm 
+static float pathX[PATH_LEN] = {455,450,10,460}; //waypoints in cm 
+static float pathY[PATH_LEN] = {495,490,10,500}; // waypoints in cm 
 float x_i; // differnce in look-ahead distance from current position  
 float y_i; // differnce in look-ahead distance from current position 
 float L_d; // Look-Ahead Distance in cm 
@@ -60,8 +61,10 @@ static constexpr int trackWidth =86;  // Wheel to Wheel in cm
 float leftMotor; 
 float rightMotor; 
 
-static constexpr int INTERPOLATION_STEP = PurePursuit::DEFAULT_INTERPOLATION_STEP;
-PurePursuit pp(&currentX, &currentY, &Azimuth, LOOK_AHEAD, INTERVAL, STOP_POINT);
+bool new_goal = false;
+
+static int INTERPOLATION_STEP =250; //mm;
+PurePursuit pp(&currentX_global, &currentY_global , &Azimuth, LOOK_AHEAD, INTERVAL, STOP_POINT);
 
 
 
@@ -74,9 +77,6 @@ void rangingHandler(UWBRangingData &rangingData) {
 
     RangingMeasures twr=rangingData.twoWayRangingMeasure();
 
-  int dist_1 = 1;
-  int dist_2 = 1;
-  int dist_3 = 1; 
 
   for (int j = 0; j < rangingData.available(); j++) {
     // skip invalid measurements
@@ -98,7 +98,8 @@ void rangingHandler(UWBRangingData &rangingData) {
   }
   
 if (!anchor1_received || !anchor2_received || !anchor3_received) {
-  return; // wait for all data
+  inRangingHandler = false;
+  return;
 }else 
 {
   // reseting bools
@@ -117,6 +118,7 @@ if (!anchor1_received || !anchor2_received || !anchor3_received) {
 
   if (fabsf(det) < 1e-6) {
     Serial.println("Error: Anchors are collinear, cannot calculate position");
+    inRangingHandler = false;
     return;
   }
   float x = (C*E - F*B) / det;
@@ -131,35 +133,8 @@ if (!anchor1_received || !anchor2_received || !anchor3_received) {
     inRangingHandler = false;
     return;
   }
-
-  currentX = x;
-  currentY = y;
-  Serial.print("Position: ");
-  Serial.print(x);
-  Serial.print(", ");
-  Serial.println(y);
-  //
-  prevX = x_circular_buffer[tail_index];
-  prevY = y_circular_buffer[tail_index];
-
-    float dx = x - prevX;
-    float dy = y - prevY;
-    float dist_sq = dx*dx + dy*dy;
-    if (dist_sq >= minMovement_sq) {
-      Azimuth = atan2f(dy, dx);
-                Serial.println(dist_sq);
-
-     }else{
-
- 
-  Serial.print("Azimuth invalid--Min Movement");
-          Serial.println(dist_sq);
-              Serial.print("Heading: ");
-    Serial.println(Azimuth);
-
-  return;
-       }
-
+// ------------ Circular Buffer Advance ------------ //
+// Advance Circular Buffer 
   head_index++;
   if (head_index == CIRCULAR_BUFFER_SIZE) {
     head_index = 0;
@@ -167,18 +142,73 @@ if (!anchor1_received || !anchor2_received || !anchor3_received) {
   tail_index++;
   if (tail_index == CIRCULAR_BUFFER_SIZE) {
     tail_index = 0;
-  } 
-
+  }
   x_circular_buffer[head_index] = x;
-  y_circular_buffer[head_index] = y;   
+  y_circular_buffer[head_index] = y;
+// ------------ End Circular Buffer Advance ------------ //
 
-  
-  //convert to degrees 
-  Azimuth=Azimuth*(180.0/PI);
-  // print the heading
-    Serial.print("Heading: ");
-    Serial.println(Azimuth);
 
+float currentX=0.0f;
+float currentY=0.0f;
+float prevX=0.0f;
+float prevY=0.0f;
+// ------------ Weighted Average ------------ //
+  float weights[HALF_CIRCULAR_BUFFER_SIZE];
+  for (int i = 0; i < HALF_CIRCULAR_BUFFER_SIZE; i++) {
+    weights[i] = 1.0f/HALF_CIRCULAR_BUFFER_SIZE;
+  }
+  // weights[] = {0.3,0.25,0.2,0.15,0.1};
+  int index = head_index;
+
+  for (int i = 0; i < HALF_CIRCULAR_BUFFER_SIZE; i++) {
+    if (index < 0) {
+      index = index + CIRCULAR_BUFFER_SIZE;
+    }
+    currentX += weights[i]*x_circular_buffer[index];
+    currentY += weights[i]*y_circular_buffer[index];
+    index--;
+  }
+
+  index = tail_index;
+  for (int i = 0; i < HALF_CIRCULAR_BUFFER_SIZE; i++) {
+    if (index >= CIRCULAR_BUFFER_SIZE) {
+      index = index - CIRCULAR_BUFFER_SIZE;
+    }
+    prevX += weights[i]*x_circular_buffer[index];
+    prevY += weights[i]*y_circular_buffer[index];
+    index++;
+  }
+    currentX_global = currentX;
+    currentY_global = currentY; 
+  // Serial.print("( ");
+  // Serial.println(currentX);
+  // Serial.print(" , ");
+  // Serial.println(currentY);
+  // Serial.print(") ");
+// ------------ End Weighted Average ------------ //
+
+// ------------ Heading Calculation ------------ //
+// Calculate the differnce in position from previous point  
+  float dx = currentX - prevX;
+  float dy = currentY - prevY;
+// Compare the dx^2+dy^2 to the distance to flag invalid headings
+  float dist_sq = dx*dx + dy*dy;
+// If the distance is greater than the minimum movement, calculate the azimuth
+  if (dist_sq >= minMovement_sq) {
+    Azimuth = atan2f(dy, dx);
+    global_azimuth = Azimuth;
+  }else{
+    // Serial.print("Azimuth invalid  ");
+    inRangingHandler = false;
+    return;
+  }
+
+  Azimuth=global_azimuth *(180.0/PI);
+  Serial.print("Heading: ");
+  Serial.println(Azimuth);
+
+  new_goal = true;
+// ------------ End Heading Calculation ------------ //
 }
 }
 
@@ -252,23 +282,44 @@ void loop() {
   digitalWrite(LEDR, !digitalRead(LEDR));
 #endif
 
-  while(inRangingHandler) {
+  while(inRangingHandler || !new_goal) {
+    
     delay(10);
   }
+  new_goal = false;
+  pp.computeNextGoalPoint();
 
-  [goal_x, goal_y] = pp.nextGoalPoint();
-    x_i=goal_x-currentX; // differnce from goal point to current position 
-    y_i=goal_y-currentY; // differnce from goal point to current position
+  float goal_x = pp.getGoalX();
+  float goal_y = pp.getGoalY();
+
+
+Serial.println(goal_x);
+Serial.println(goal_y);
+
+    x_i=goal_x-currentX_global; // differnce from goal point to current position 
+    y_i=goal_y-currentY_global; // differnce from goal point to current position
 // Find Look-ahead Distance 
+float DesiredHeading=0.0f;
+DesiredHeading=atan2f(y_i, x_i);
+DesiredHeading=DesiredHeading*(180.0/PI);
+Serial.print("Desired Heading: ");
+Serial.println(DesiredHeading);
+  Serial.println(currentX);
+  Serial.println(currentY);
+// Serial.print("Global Heading: ");
+// Serial.println(global_azimuth);
 L_d2=x_i*x_i+y_i*y_i;  // Look-ahead Distance Squared 
 L_d=sqrt(L_d2); // Look-ahead distance
+
 // Compute the Curvature Coeff (K)
-K=2*sinf(Azimuth)/L_d; 
-omega=K*Velocity; 
+K=2*sinf(global_azimuth)/L_d; 
+Serial.print("Curvature Coeff: ");
+Serial.println(K);
+omega=K*velocity; 
 
 // // Find Motor Velocities 
 // leftMotor=(velocity+omega*trackWidth/2)/wheelRadius; 
 // rightMotor=(velocity+omega*trackWidth/2)/wheelRadius; 
-
-  delay(1000);
+// Serial.println(leftMotor);
+// Serial.println(rightMotor);
 }
--- UWB/TWR_Round_Robin_02/RR_TAG_v2/RR_Tag_v2/RR_Tag_v2.ino
+++ UWB/TWR_Round_Robin_02/RR_TAG_v2/RR_Tag_v2/RR_Tag_v2.ino
@@ -1,45 +1,46 @@
 #include <PortentaUWBShield.h>
+#include <PurePursuit.h>
 #include <math.h>
 
 
 /**
- * This Script claculates the position of an Arduino Protenta Tag based 
- * on the distances to three anchors. Using multicast to range with 3+ anchors
- * UWB Ranging Controller (one-to-many)*
- * It expects multiple counterparts setup as Responders/Controlees*
+ * 
  */
 
 // Anchor Locations in Centimeters (x,y) z=0 
-const float Anchor1_x=1;// cm 
-const float Anchor1_y=1; // cm 
-const float Anchor2_x=1; // cm 
-const float Anchor2_y=213; // cm 
-const float Anchor3_x=182; // cm 
-const float Anchor3_y=1; // cm 
-
+const float Anchor1_x=0;// cm 
+const float Anchor1_y=0; // cm 
+const float Anchor2_x=0; // cm 
+const float Anchor2_y=467; // cm 
+const float Anchor3_x=506; // cm 
+const float Anchor3_y=0; // cm 
+// Initialize Distance Variables
+int dist_1 = 0;
+int dist_2 = 0;
+int dist_3 = 0; 
+// Anchor received state variables
 bool anchor1_received = false;
 bool anchor2_received = false;
 bool anchor3_received = false;
+
 //previous position state variable 
-float prevX = 0.0f;// cm 
-float prevY = 0.0f;// cm 
-float currentX = 0.0f;// cm 
-float currentY = 0.0f;// cm 
+float currentX_global = 0.0f;// cm 
+float currentY_global = 0.0f;// cm 
 bool prev_valid = false;
 
 // Heading State Variable 
 float Azimuth = 0.0f; // rad
 
 // constants 
+static constexpr int HALF_CIRCULAR_BUFFER_SIZE = 5;
+static constexpr int CIRCULAR_BUFFER_SIZE = HALF_CIRCULAR_BUFFER_SIZE*2;
 const float MinMovement = 5.0f; // cm 
 const float minMovement_sq=MinMovement*MinMovement; // minimum movement squared
-float x_circular_buffer[5];
-float y_circular_buffer[5];
-static constexpr int CIRCULAR_BUFFER_SIZE = 5;
+float x_circular_buffer[CIRCULAR_BUFFER_SIZE];
+float y_circular_buffer[CIRCULAR_BUFFER_SIZE];
 int head_index = 0;
 int tail_index =  CIRCULAR_BUFFER_SIZE - 1;
 bool inRangingHandler = false;
-
 
 // handler for ranging notifications
 void rangingHandler(UWBRangingData &rangingData) {
@@ -48,10 +49,6 @@ void rangingHandler(UWBRangingData &rangingData) {
   {
 
     RangingMeasures twr=rangingData.twoWayRangingMeasure();
-
-  int dist_1 = 1;
-  int dist_2 = 1;
-  int dist_3 = 1; 
 
   for (int j = 0; j < rangingData.available(); j++) {
     // skip invalid measurements
@@ -73,14 +70,16 @@ void rangingHandler(UWBRangingData &rangingData) {
   }
   
 if (!anchor1_received || !anchor2_received || !anchor3_received) {
+  inRangingHandler = false;
   return; // wait for all data
 }else 
 {
-  // reseting bools
+// reseting bools
   anchor1_received = false;
   anchor2_received = false;
   anchor3_received = false;
 
+// ------------ Trilateration Calculation ------------ //
   float A = 2.0f*Anchor2_x - 2.0f*Anchor1_x; 
   float B = 2.0f*Anchor2_y - 2.0f*Anchor1_y; 
   float C = dist_1*dist_1 - dist_2*dist_2 - Anchor1_x*Anchor1_x + Anchor2_x*Anchor2_x - Anchor1_y*Anchor1_y + Anchor2_y*Anchor2_y; 
@@ -96,9 +95,19 @@ if (!anchor1_received || !anchor2_received || !anchor3_received) {
 
     return;
   }
+  
   float x = (C*E - F*B) / det;
   float y = (A*F - C*D) / det;
 
+// ------------ End Trilateration Calculation ------------ //
+
+  // Serial.print("Position: ");
+  // Serial.print(x);
+  // Serial.print(", ");
+  // Serial.println(y);
+
+
+// populate circular buffer for n valid data points 
   if(!prev_valid) {
     for(int i = 0; i < CIRCULAR_BUFFER_SIZE; i++) {
       x_circular_buffer[i] = x;
@@ -108,58 +117,101 @@ if (!anchor1_received || !anchor2_received || !anchor3_received) {
     inRangingHandler = false;
     return;
   }
-  
-  currentX = x;
-  currentY = y;
-  Serial.print("Position: ");
-  Serial.print(x);
-  Serial.print(", ");
-  Serial.println(y);
-  //
-  prevX = x_circular_buffer[tail_index];
-  prevY = y_circular_buffer[tail_index];
+// ------------ Circular Buffer Advance ------------ //
+// Advance Circular Buffer 
+head_index++; // head_index + 1 
+if (head_index == CIRCULAR_BUFFER_SIZE) { // when index = to buffer size reset to zero
+  head_index = 0;
+}
+tail_index++;
+if (tail_index == CIRCULAR_BUFFER_SIZE) {
+  tail_index = 0;
+} 
+// Set head_index to current postion
+x_circular_buffer[head_index] = x; 
+y_circular_buffer[head_index] = y;
+// ------------ End Circular Buffer Advance ------------ //
 
-    float dx = x - prevX;
-    float dy = y - prevY;
+float currentX=0.0f;
+float currentY=0.0f;
+float prevX=0.0f;
+float prevY=0.0f;
+
+
+
+  // currentX = x;
+  // currentY = y;
+  // // Assign prevX to the tail of buffer 
+  // prevX = x_circular_buffer[tail_index];
+  // prevY = y_circular_buffer[tail_index];
+
+
+// ------------ Weighted Average ------------ //
+float weights[HALF_CIRCULAR_BUFFER_SIZE];
+for (int i = 0; i < HALF_CIRCULAR_BUFFER_SIZE; i++) {
+  weights[i] = 1.0f/HALF_CIRCULAR_BUFFER_SIZE;
+}
+// weights[] = {0.3,0.25,0.2,0.15,0.1};
+  int index = head_index;
+
+  for (int i = 0; i < HALF_CIRCULAR_BUFFER_SIZE; i++) {
+    if (index < 0) {
+      index = index + CIRCULAR_BUFFER_SIZE;
+    }
+ // weighted average
+     currentX += weights[i]*x_circular_buffer[index];
+     currentY += weights[i]*y_circular_buffer[index];
+
+    index--;
+  }
+
+  index = tail_index;
+for (int i = 0; i < HALF_CIRCULAR_BUFFER_SIZE; i++) {
+  if (index >= CIRCULAR_BUFFER_SIZE) {
+    index = index - CIRCULAR_BUFFER_SIZE;
+  }
+   // weighted average
+    prevX += weights[i]*x_circular_buffer[index];
+    prevY += weights[i]*y_circular_buffer[index];
+  index++;
+}
+currentX_global = currentX;
+currentY_global = currentY;
+  Serial.print("( ");
+  Serial.println(currentX);
+  Serial.print(" , ");
+  Serial.println(currentY);
+  Serial.print(") ");
+// ------------ End Weighted Average ------------ //
+
+// ------------ Heading Calculation ------------ //
+
+// Calculate the differnce in position from previous point
+    float dx = currentX - prevX;
+    float dy = currentY - prevY;
+// Compare the dx^2+dy^2 to the distance to flag invalid headings
     float dist_sq = dx*dx + dy*dy;
-    if (dist_sq >= minMovement_sq) {
+    if (dist_sq >= minMovement_sq) { // heading is valid calcualte azimuth
       Azimuth = atan2f(dy, dx);
-                Serial.println(dist_sq);
-
      }else{
-
- 
-  Serial.print("Azimuth invalid--Min Movement");
-          Serial.println(dist_sq);
-              Serial.print("Heading: ");
-    Serial.println(Azimuth);
-
+  Serial.print("Azimuth invalid  ");
+  inRangingHandler = false;
   return;
        }
-
-  head_index++;
-  if (head_index == CIRCULAR_BUFFER_SIZE) {
-    head_index = 0;
-  }
-  tail_index++;
-  if (tail_index == CIRCULAR_BUFFER_SIZE) {
-    tail_index = 0;
-  } 
-  x_circular_buffer[head_index] = x;
-  y_circular_buffer[head_index] = y;   
-
-
+   
 //convert to degrees 
 Azimuth=Azimuth*(180.0/PI); 
   // print the heading
-    Serial.print("Heading: ");
-    Serial.println(Azimuth);
-//Serial.println(prevX);
-//Serial.println(prevY);
+  Serial.print("Heading: ");
+  Serial.println(Azimuth);
+  
+    
+// ------------ End Heading Calculation ------------ //
+
 }
 }
 
-  inRangingHandler = false;
+  inRangingHandler = false; // reset to 0 
 }
 
 void setup() {
