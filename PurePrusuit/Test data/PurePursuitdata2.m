c97a7e855b2b4d5b207e9261be9a37516df6a54e
granhunter <35352320+granhunter@users.noreply.github.com>
2026-03-23 20:33:09 -0400

    take2

---
+++ PurePrusuit/Test data/PurePursuitdata2.m
@@ -0,0 +1,49 @@
+clc
+clear
+close all
+
+%% incoming test
+% what I need: 
+% robot x and y
+% goal x and y
+% radius of curvature
+% heading (angle from goal) and azimuth (angle in room)
+
+
+%% Canada test1
+mapArea = [0 0;
+    70*2.54*12 0;
+    70*2.54*12 32.4*2.54*12;
+    (70-2)*2.54*12 (32.4)*2.54*12;
+    (70-2)*2.54*12 (32.4+19.81)*2.54*12;
+    (70-2-12.5)*2.54*12 (32.4+19.81)*2.54*12;
+    (70-2-12.5)*2.54*12 (32.4+19.81+14.75)*2.54*12;
+    (70-2-12.5-4.45)*2.54*12 (32.4+19.81+14.75)*2.54*12;
+    (70-2-12.5-4.45)*2.54*12 (32.4+19.81+14.75+20)*2.54*12;
+    0 (32.4+19.81+14.75+20)*2.54*12;
+    0 0];
+
+data5_1= importdata("test_3_24_6");
+data5_1 = data5_1.data;
+robotx = [];
+roboty = [];
+Azimuth = [];
+i = 1;
+while i <= height(data5_1)-3
+    robotx = [robotx; data5_1(i,1)];
+    roboty = [roboty; data5_1(i+1,1)];
+    Azimuth = [Azimuth; data5_1(i+2,1)];
+    i = i+3;
+end
+
+figure
+hold on
+scatter(robotx,roboty)
+title("Reference Positioning Test Canada")
+xlabel("x position (cm)")
+ylabel("y position (cm)")
+legend("Robot Position")
+hold off
+
+
+
